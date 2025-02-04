3a56ea14a6ef57b5bf99ca5e5c204bd0bf6c3de6
miroslavmadzharov02 <miroslavmadzharov02@gmail.com>
2025-02-04 23:39:11 +0200

    attempt to make enemy prioritize tiles

--- src/enemy.py
+++ src/enemy.py
@@ -17,7 +17,13 @@ class Enemy(Entity):
         self.enemy_tile = Tile.PLAYER
     
     def pick_direction(self, level, center_x, center_y):
-        def get_next_tile_position(level, center_x, center_y, direction):
+        if self.turns_allowed[self.previous_direction]:
+            if random.random() > 0.01:
+                self.direction = self.previous_direction
+                return
+
+
+        def get_next_tile(level, center_x, center_y, direction):
             fudge = 15
             if direction == self.Direction.UP:
                 return level[(center_y - fudge) // self.square_size][center_x // self.square_size]
@@ -29,22 +35,21 @@ class Enemy(Entity):
                 return level[center_y // self.square_size][(center_x + fudge) // self.square_size]
             return None
 
-        if self.turns_allowed[self.previous_direction]:
-            self.direction = self.previous_direction
-            return
-
+        valid_directions = []
         for i in range(len(self.Direction)):
             if self.turns_allowed[i]:
-                next_tile = get_next_tile_position(level, center_x, center_y, i)
-                if next_tile == self.enemy_tile or next_tile == Tile.EMPTY:
-                    self.direction = i
-                    self.previous_direction = i
-                    return
+                next_tile = get_next_tile(level, center_x, center_y, i)
+                if next_tile == self.enemy_tile.board_index or next_tile == Tile.EMPTY.board_index:
+                    valid_directions.append(i)
 
-        allowed_directions = [i for i in range(len(self.Direction)) if self.turns_allowed[i]]
-        if allowed_directions:
-            self.direction = random.choice(allowed_directions)
+        if valid_directions:
+            self.direction = random.choice(valid_directions)
             self.previous_direction = self.direction
+        else:
+            allowed_directions = [i for i in range(len(self.Direction)) if self.turns_allowed[i]]
+            if allowed_directions:
+                self.direction = random.choice(allowed_directions)
+                self.previous_direction = self.direction
 
     def move(self, level, center_x, center_y):
         self.check_position(level, center_x, center_y)
