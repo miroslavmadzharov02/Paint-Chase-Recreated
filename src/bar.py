5cee845936888bc478063b6f79d80a4d83b8b126
miroslavmadzharov02 <miroslavmadzharov02@gmail.com>
2025-02-05 13:48:34 +0200

    updated bar color and height

--- src/bar.py
+++ src/bar.py
@@ -10,16 +10,16 @@ class Bar:
         self.bar_height = BOTTOM_PADDING
         self.total_time = SECONDS * 1000
         self.start_ticks = pygame.time.get_ticks()
-        self.main_color = 'forestgreen'
-        self.alt_color = 'goldenrod'
+        self.main_color = 'gray17'
+        self.alt_color = 'ghostwhite'
 
     def draw(self, screen):
         elapsed_time = pygame.time.get_ticks() - self.start_ticks
         progress_ratio = min(elapsed_time / self.total_time, 1)
         progress_width = int(self.bar_width * progress_ratio)
 
-        pygame.draw.rect(screen, self.main_color, (self.bar_x, self.bar_y, self.bar_width, self.bar_height), 2)
-        pygame.draw.rect(screen, self.alt_color, (self.bar_x, self.bar_y, progress_width, self.bar_height))
+        pygame.draw.rect(screen, self.main_color, (self.bar_x, self.bar_y, self.bar_width, self.bar_height), 5)
+        pygame.draw.rect(screen, self.alt_color, (self.bar_x, self.bar_y + 5, progress_width, self.bar_height - 10))
 
     def is_time_over(self):
         elapsed_time = pygame.time.get_ticks() - self.start_ticks
