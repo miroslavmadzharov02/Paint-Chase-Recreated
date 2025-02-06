77f3a7f3b1ce4069c7f2b5395110313bb53151b6
miroslavmadzharov02 <miroslavmadzharov02@gmail.com>
2025-02-06 23:32:13 +0200

    tile unit tests

---
+++ tests/test_tile.py
@@ -0,0 +1,36 @@
+import pytest
+from src.entity import Entity
+from src.tile import Tile, is_empty_tile, is_paintable_tile, is_boost_tile
+
+def test_tile_enum_values():
+    for tile in Tile.__members__.values():
+        assert isinstance(tile.board_index, int)
+        assert isinstance(tile.color, str)
+
+@pytest.mark.parametrize("tile, expected", [
+        (Tile.EMPTY.board_index, True),
+        (Tile.ENEMY.board_index, True),
+        (Tile.PLAYER.board_index, True),
+        (Tile.RESPAWN.board_index, True),
+        (Tile.BOOST.board_index, True),
+        (Tile.WALL.board_index, False)
+    ])
+def test_is_empty_tile(tile, expected):
+    assert is_empty_tile(tile) == expected
+
+@pytest.mark.parametrize("tile, entity_enemy_tile, expected", [
+        (Tile.EMPTY.board_index, Tile.ENEMY, True),
+        (Tile.ENEMY.board_index, Tile.ENEMY, True),
+        (Tile.WALL.board_index, Tile.ENEMY, False),
+        (Tile.BOOST.board_index, Tile.ENEMY, False),
+        (Tile.RESPAWN.board_index, Tile.ENEMY, False)
+    ])
+def test_is_paintable_tile(mocker, tile, entity_enemy_tile, expected):
+    entity_mock = mocker.Mock()
+    entity_mock.enemy_tile = entity_enemy_tile
+    assert is_paintable_tile(tile, entity_mock) == expected
+
+@pytest.mark.parametrize("tile, expected", 
+    [(tile.board_index, tile == Tile.BOOST) for tile in Tile.__members__.values()])
+def test_is_boost_tile(tile, expected):
+    assert is_boost_tile(tile) == expected
